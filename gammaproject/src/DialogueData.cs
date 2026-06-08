00ad72a35408f53471e47fd89c40c4d20e17c52e
The Coward <kotagi@tutanota.com>
2026-06-08 08:45:55 -0500

    new animation

--- gammaproject/src/DialogueData.cs
+++ gammaproject/src/DialogueData.cs
@@ -4,7 +4,7 @@ using System;
 namespace Gamma {
     public partial class Main : Node {
         public static DialogueData testDialogueData = new DialogueData {
-            speakerPortrait = GD.Load<Texture2D>("res://assets/textures/dialogueportraits/spritesheet.png"),
+            speakerPortrait = GD.Load<Texture2D>("res://assets/textures/dialogueportraits/CHR_SLINK_ANIMATIONSHEET_01.png"),
             speakerName = "TEST",
             text = "This is a test dialogue. This is a test dialogue. This is a test dialogue. This is a test dialogue.",
             onDialogueStart = new Action<Main>[] {
